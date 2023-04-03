f98aba2809357249f08a9288503bc33bcb843300
Amine derbal <aminedoran31@gmail.com>
2023-04-03 16:51:59 +0100

    display initial state books

--- src/components/BookLists.js
+++ src/components/BookLists.js
@@ -1,31 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
 const BookLists = () => {
-  const books = [
-    {
-      id: 1,
-      title: 'Pride and Prejudice',
-      author: 'Jane Austen',
-    },
-    { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-    { id: 3, title: 'One Hundred Years of Solitude', author: 'Gabriel García Márquez' },
-    {
-      id: 4,
-      title: 'In Cold Blood',
-      author: 'Truman Capote',
-    },
-    {
-      id: 5,
-      title: 'Wide Sargasso Sea',
-      author: 'Jean Rhys',
-    },
-    {
-      id: 6,
-      title: 'Brave New World',
-      author: 'Aldous Huxley',
-    },
-  ];
+  const { books } = useSelector((store) => store.book);
 
   return (
     <div>
