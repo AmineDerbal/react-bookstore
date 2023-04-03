37f3d676d163bf14b2e08505ba37c208c2c65585
Amine derbal <aminedoran31@gmail.com>
2023-04-03 14:28:52 +0100

    add slice for books

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  books: [],
+};
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, { payload }) => {
+      state.books.push({ title: payload.title, author: payload.author });
+    },
+    removeBook: (state, { payload }) => {
+      state.books.filter((book) => book.id !== payload.id);
+    },
+  },
+});
+
+export const { addBook, removeBook } = booksSlice.actions;
+export default booksSlice.reducer;
