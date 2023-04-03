f3c5e02b0cf4f3fadeba589408c34e221261c238
Amine derbal <aminedoran31@gmail.com>
2023-04-03 18:27:40 +0100

    fix linter

--- src/components/BookLists.js
+++ src/components/BookLists.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import Book from './book';
+import Book from './Book';
 
 const BookLists = () => {
   const { books } = useSelector((store) => store.book);
