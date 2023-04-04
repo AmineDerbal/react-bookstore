296e295475a3a43900219aed21feb0c0ce5efd0a
Amine derbal <aminedoran31@gmail.com>
2023-04-04 12:46:37 +0100

    add post book function

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/books/booksSlice';
+import { postBookToApi } from '../redux/books/booksSlice';
 
 const AddBook = () => {
   const initialInputs = {
@@ -21,7 +21,7 @@ const AddBook = () => {
     e.preventDefault();
     const id = uuidv4();
     const book = { id, ...input };
-    dispatch(addBook(book));
+    dispatch(postBookToApi(book));
     setInputs(initialInputs);
   };
   return (
